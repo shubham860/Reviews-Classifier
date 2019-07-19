148f626a23d55b08a8eb6ed0b91f97f72f276985
shubham860 <shubham.chauhan860@gmail.com>
2019-07-19 20:33:31 +0530

    update

--- Cetpa-reviews.py
+++ Cetpa-reviews.py
@@ -14,7 +14,6 @@ Time = []
 Rating_star=[]
 
 url = "https://www.google.com/maps/place/CETPA+INFOTECH+PVT+LTD/@28.5826801,77.3138382,19.96z/data=!4m7!3m6!1s0x390ce45fa267c455:0x20e02d4622605a21!8m2!3d28.5825324!4d77.3160029!9m1!1b1?hl=en-IN"
-#url = "https://www.google.com/maps/place/CETPA+INFOTECH+PVT+LTD/@28.5825653,77.3138356,17z/data=!4m7!3m6!1s0x390ce45fa267c455:0x20e02d4622605a21!8m2!3d28.5825324!4d77.3160029!9m1!1b1"
 driver.get(url)
 content = driver.page_source
 
